c6bcf5ce4b5d135e1203635854f5cf7b47142587
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-04-25 20:04:02 +0530

    single duplicate in array

---
+++ duplicateelements.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int duplicate(int arr[], int n)
+{
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        ans = ans ^ i;
+    }
+    return ans;
+}
+int main()
+{
+    int size;
+    cout << "Enter the size of ARRAY:" << endl;
+    cin >> size;
+    int arr[100] = {0};
+    cout << "Enter the ARRAY elements:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "The duplicate element is:" << duplicate(arr, size);
+}
