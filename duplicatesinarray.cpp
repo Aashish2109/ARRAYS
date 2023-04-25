4dddafe8d1117018f41f25556d60beaf6ce20169
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-04-25 20:04:19 +0530

    all duplicates in array

---
+++ duplicatesinarray.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> duplicates(vector<int> &arr)
+{
+    int n = arr.size() - 1;       // FOR CALCULATING THE SIZE OF ARRAY
+    int *freq = new int[n + 1](); // FOR CREATING A FREQ ARRAY OF SIZE N + 1
+    vector<int> ans;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        freq[arr[i]]++;
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (freq[arr[i]] > 1)
+        {
+            ans.push_back(arr[i]);
+        }
+    }
+    if (arr.size() == 0)
+        return {-1};
+    return ans;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of ARRAY :" << endl;
+    cin >> size;
+    vector<int> arr(size);
+    cout << "Enter the ARRAY elements:" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> answer = duplicates(arr);
+    if (answer.empty() && -1)
+    {
+        cout << "NO DUPLICATES";
+    }
+    else
+        cout << "DUPLICATES ARE:";
+    for (int i = 0; i < answer.size(); i++)
+    {
+
+        {
+            cout << answer[i] << " ";
+        }
+    }
+
+    return 0;
+}
