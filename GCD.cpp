3f8cf7e809701c9f35b8ce17fa70c9f86a0b7986
Aashish Gupta <99539876+Aashish2109@users.noreply.github.com>
2023-04-26 23:21:22 +0530

    lcm and hcf

---
+++ GCD.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else if (a == 0)
+    {
+        return b;
+    }
+    else
+        return gcd(b % a, a); // Either this or
+    // return gcd(a, a % b); // This
+}
+int LCM(int a, int b)
+{
+    int LCM = a * b / gcd(a, b);
+    return LCM;
+}
+int main()
+{
+    int a, b;
+    cout << "Enter the first number:" << endl;
+    cin >> a;
+    cout << "Enter the second number:" << endl;
+    cin >> b;
+    // cout << "The GCD of " << a << " and " << b << " is " << gcd(a, b);
+    cout << "The LCM of " << a << " and " << b << " is " << LCM(a, b);
+}
